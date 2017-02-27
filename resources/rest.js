d1ef6d782973603ecfa23ff9c0c7259e91eddbc6
Adrian <adrian@Adrians-DowntownDC-iMac.local>
2017-02-27 17:19:33 -0500

    Showing more busses

--- resources/rest.js
+++ resources/rest.js
@@ -67,7 +67,7 @@ module.exports = function(app) {
             const options = {
                 hostname: 'api.wmata.com',
                 port: 443,
-                path: `/Bus.svc/json/jBusPositions?RouteID=X2&Lat=38.9001&Lon=-77.0293&Radius=1600${getWmataApiKey()}`,
+                path: `/Bus.svc/json/jBusPositions?RouteID&Lat=38.9001&Lon=-77.0293&Radius=1600${getWmataApiKey()}`,
                 method: 'GET'
             }
 
