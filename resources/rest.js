c345dd1c730624fcbc4ea69ef175ec63332f6531
Adrian <adrian@Adrians-DowntownDC-iMac.local>
2017-02-28 10:26:46 -0500

    Shorten Gallery Place Metro name

--- resources/rest.js
+++ resources/rest.js
@@ -42,7 +42,7 @@ module.exports = function(app) {
         },
         F01: { // Gallery Place
             Predictions: [],
-            StationName: 'Gallery Place / Chinatown'
+            StationName: 'Gallery Place'
         },
         liveBusses: [],
         bikeshare: [],
